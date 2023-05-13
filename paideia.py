187ccef0ad1a941bf322b14cf565de6c393b816e
Norris36 <jensbay@outlook.dk>
2023-05-13 11:52:43 +0200

    firs

---
+++ paideia.py
@@ -0,0 +1,21 @@
+##
+## Write a streamlit home page for my paideia project. The goal is to create a webstite that can help users learn about their subjects quicker
+##
+
+import streamlit as st
+
+# Title
+st.title("Paideia")
+
+# Header
+st.header("Welcome to Paideia")
+
+# Subheader
+st.subheader("A website to help you learn about your subjects quicker")
+
+# Text
+st.text("This is a text")
+
+# Markdown
+st.markdown("### This is a markdown")
+
