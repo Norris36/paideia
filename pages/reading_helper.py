d5295bfbaa96a16703fd4d64087c0b7b5ba3835b
Norris36 <jensbay@outlook.dk>
2023-12-18 21:20:03 -0600

    re-installed the requirements

---
+++ pages/reading_helper.py
@@ -0,0 +1,2 @@
+## This should be a webb site that reads the text from diffèrent authors and then shows, each of them will have a read button
+## if that is pressed, then we will log it in a database and then we will show the next one
